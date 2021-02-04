12230919e9deeeda24a2447259fa21c601d8f9e9
amit-web <mehtaamit9334@gmail.com>
2021-02-05 02:52:06 +0530

    Pricing page By Amit Mehta

---
+++ pricing.js
@@ -0,0 +1,104 @@
+var price_button_1 = document.getElementById("btn_pricing")
+function changecolour(){
+   price_button_1.style.backgroundColor="rgb(10, 12, 114)"
+  
+   
+}
+
+function chnage_text_color(){
+   price_button_1.style.color="white"
+}
+
+function removecolor(){
+    price_button_1.style.color="rgb(10, 12, 114)"
+}
+
+function removebackground_color(){
+    price_button_1.style.backgroundColor="white"
+}
+
+ price_button_1.addEventListener('mouseover',changecolour)
+ price_button_1.addEventListener('mouseover',chnage_text_color)
+ price_button_1.addEventListener('mouseleave',removecolor)
+ price_button_1.addEventListener('mouseleave',removebackground_color)
+
+
+
+ var price_button = document.getElementById("btn_pricing_1")
+  function changecolour_1(){
+   price_button.style.backgroundColor="rgb(10, 12, 114)"
+  }
+function chnage_text_color_1(){
+   price_button.style.color="white"
+}
+
+function removecolor_1(){
+    price_button.style.color="rgb(10, 12, 114)"
+}
+
+function removebackground_color_1(){
+    price_button.style.backgroundColor="white"
+}
+
+ price_button.addEventListener('mouseover',changecolour_1)
+ price_button.addEventListener('mouseover',chnage_text_color_1)
+ price_button.addEventListener('mouseleave',removecolor_1)
+ price_button.addEventListener('mouseleave',removebackground_color_1)
+
+
+
+ 
+ 
+ 
+ var price_button_2 = document.getElementById("btnprice_2")
+
+
+   
+function changecolour_2(){
+   price_button_2.style.backgroundColor="rgb(10, 12, 114)"
+  
+   
+}
+
+function chnage_text_color_2(){
+   price_button_2.style.color="white"
+}
+
+function removecolor_2(){
+    price_button_2.style.color="rgb(10, 12, 114)"
+}
+
+function removebackground_color_2(){
+    price_button_2.style.backgroundColor="white"
+}
+
+ price_button_2.addEventListener('mouseover',changecolour_2)
+ price_button_2.addEventListener('mouseover',chnage_text_color_2)
+ price_button_2.addEventListener('mouseleave',removecolor_2)
+ price_button_2.addEventListener('mouseleave',removebackground_color_2)
+
+
+     var signupfree_button = document.getElementById("signup_1")
+
+     function changedarkblue (){
+         signupfree_button.style.backgroundColor="rgb(13, 25, 80)"
+     }
+     function removedarkblue(){
+       signupfree_button.style.backgroundColor="rgb(44, 62, 143)"
+     }
+
+     signupfree_button.addEventListener('mouseover',changedarkblue)
+     signupfree_button.addEventListener('mouseleave',removedarkblue)
+
+
+     var signupfree_button_1 = document.getElementById("signup_lastbtn")
+
+     function changedarkblue_1 (){
+         signupfree_button_1.style.backgroundColor="rgb(13, 25, 80)"
+     }
+     function removedarkblue_1(){
+       signupfree_button_1.style.backgroundColor="rgb(44, 62, 143)"
+     }
+
+     signupfree_button_1.addEventListener('mouseover',changedarkblue_1)
+     signupfree_button_1.addEventListener('mouseleave',removedarkblue_1)
