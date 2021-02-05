2c7052041a270f7beb3ad3d51e178e1a213e6a10
nil patil <nilp7744@gmail.com>
2021-02-05 22:33:23 +0530

    externalbuttondonebynil

---
+++ EXTERNALBUTTON.js
@@ -0,0 +1,9 @@
+document.getElementById("btn1").addEventListener("mouseover",function(){
+    var box1 = document.getElementById("box")
+    if(box1.style.display == "none"){
+        box1.style.display = "block"
+    }
+    else{
+        box1.style.display = "none"
+    }
+ })
